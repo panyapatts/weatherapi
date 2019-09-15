4e98bdbe4951877bac8442b47bce17fd708fc0e4
panyapatts <55220122+panyapatts@users.noreply.github.com>
2019-09-15 07:46:46 +0700

    Update requestweather.py

--- requestweather.py
+++ requestweather.py
@@ -1,4 +1,4 @@
 import urllib.request, json 
-with urllib.request.urlopen("http://maps.googleapis.com/maps/api/geocode/json?address=google") as url:
+with urllib.request.urlopen("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22") as url:
     data = json.loads(url.read().decode())
     print(data)
